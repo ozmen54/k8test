70188e77283217f5eac4c1f9d3fe8df8018630d8
Ahmet Özmen <ozmen@sakarya.edu.tr>
2025-05-15 10:56:56 +0300

    Initial commit

---
+++ src/main/java/com/sau/k8test/K8testApplication.java
@@ -0,0 +1,13 @@
+package com.sau.k8test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K8testApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(K8testApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sau/k8test/K8testApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sau.k8test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8testApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
